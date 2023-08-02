65e2ef465e6b2a7af0d66cba293b009c3cb4c25a
JeanLamego <jeanlamego@hotmail.com>
2023-08-02 20:01:29 -0300

    fazendo o css

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,32 @@
 import { useState } from 'react'
+import axios from 'axios'
+
+import './App.css'
 
 
 function App() {
 
-  // const key = https://api.openweathermap.org/data/2.5/weather?q=japan&units=metric&appid=de44d6a961bec9dd4dce6385403fe381&lang=pt_br;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=japan&units=metric&appid=de44d6a961bec9dd4dce6385403fe381&lang=pt_br`
 
   // https://source.unsplash.com/414x896/?riodejaneiro
 
   return (
     <div className="App">
-      <header>
-        <div className="location">Rio de Janeiro</div>
-        <div className="clock">18:11</div>
-      </header>
+      <div className='header'>
+        <h2 className="location">Rio de Janeiro</h2>
+        <p className="clock">18:11</p>
+      </div>
       <div className="mid-container">
         <input type="text" 
         placeholder='Escolha o local'
         />
       </div>
-      <bottom>
-        <div className="weather-conditions">Clear</div>
-        <div className="feelslike">50c</div>
-        <div className="humidity">50%</div>
-        <div className="temperature">34C</div>
-      </bottom>
+      <div className='bottom'>
+        <p className="weather-conditions">Clear</p>
+        <p className="feelslike">50ºc</p>
+        <p className="humidity">50%</p>
+        <h2 className="temperature">34ºC</h2>
+      </div>
     </div>
   )
 }
